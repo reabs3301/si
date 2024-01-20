24a0875dc14f9c6f3e5516b72e7d4ae851ab076b
rem_zi <97393585+reabs3301@users.noreply.github.com>
2024-01-20 09:26:09 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('delete_product/<int:id>/<str:type>',views.delete_product , name = 'delete'),
     path('search_product/',views.search_product , name = 'search_product'),
     path('choose_add/',views.choosing , name='choose'),
+    path('details_centre/<int:id>',views.details , name='details'),
+    path('add_vente/',views.add_vente , name='add_vente'),
+    path('table_team/<int:id>',views.afficher_team , name = 'list_team'),
+    path('select_team/<int:id>',views.select_team , name = 'select_team'),
+    path('pointage/<int:id>',views.pointage , name = 'pointage'),
+    path('emprunt/<int:id>',views.emprunt , name = 'emprunt'),
 ]
